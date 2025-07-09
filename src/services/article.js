0b7bec8451ccc518d9672b395b3f1aa4f6ca8613
Harshit Soni <149086840+harshitsoni341@users.noreply.github.com>
2025-07-10 01:42:25 +0530

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -5,9 +5,9 @@ const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        baseUrl: ''https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Key', 67e9e8c4e4msh0dd281e516f320ep19c539jsn13256de62b12);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
